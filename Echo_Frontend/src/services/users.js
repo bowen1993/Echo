1582ca0e59619c5238c8f6a3e2f2d13aab090988
段荣 <dr@terminus.io>
2017-06-30 16:10:25 +0800

    warp request func

--- Echo_Frontend/src/services/users.js
+++ Echo_Frontend/src/services/users.js
@@ -1,13 +1,13 @@
 import request from 'utils/request';
 
 export async function query() {
-  return request('/api/users');
+  return request.get('/api/users');
 }
 
 export async function checkLogin(params) {
-  return request('/api/users/login_success', { method: 'POST', headers: { 'Content-type': 'application/json' }, body: JSON.stringify(params) });
+  return request.post('/api/users/login_success', params);
 }
 
 export const getCsrf = async () => {
-  return request('/api/users/getCsrf');
+  return request.get('/api/users/getCsrf');
 };
