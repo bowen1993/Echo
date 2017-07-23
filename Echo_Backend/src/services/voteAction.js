8e6b5d04faf0654b3d17a10ad3827b3a49134d51
ideaworld <gbwlovehp@gmail.com>
2017-07-23 11:02:17 +0800

    add answer vote

--- Echo_Backend/src/services/voteAction.js
+++ Echo_Backend/src/services/voteAction.js
@@ -1,8 +1,18 @@
 import model from '../models';
+import answerAction from './answerAction'
 
 const Answer = model.Answer;
 
 export const vote = async (answerId, up, down) => {
   const session = await model.getSession();
   const answerDao = session.getDao(Answer);
+
+  const answerObj = await answerAction.getAnswerObjectById(answerId);
+
+  if( answerObj ){
+    answerObj.up = answerObj.up + up;
+    answerObj.down = answerObj.down + down;
+    await answerDao.updateOne(answerObj);
+  }
+
 };
