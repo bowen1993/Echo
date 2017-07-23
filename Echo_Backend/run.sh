fd2f21e6555d4e5e3a2d477ecffa1479961e4b63
ideaworld <gbwlovehp@gmail.com>
2017-07-23 23:25:49 +0800

    update deployment configure

--- Echo_Backend/run.sh
+++ Echo_Backend/run.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
+echo "Start Redis Server..."
+redis-server &
 echo "Start MongoDB"
 mongod --quiet &
 sleep 2;
-cd /root/s4gserver
+cd /root/Echo_Backend
 echo "MongoDB started, loading init data...."
 python load_data.py
 echo "Data loaded, init norch search"
